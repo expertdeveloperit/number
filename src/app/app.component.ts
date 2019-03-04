05b25170cb43bb6ebd130d914b45e807c32367bb
Gurujot Singh <mrmaan4u@gmail.com>
2019-03-04 17:02:08 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-app';
+  bgBlur = false;
+  public result = {
+    position1: '1st place',
+    position2: '2nd place',
+    position3: '3rd place',
+    position4: '4th place',
+    position5: '5th place'
+  };
+  constructor(private httpClient: HttpClient) {}
+
+  loadPhoneNumber(positionVal) {
+    const campaignData = {
+      campaignCode : 'pm100',
+      position : positionVal
+    };
+    this.bgBlur = true;
+    this.httpClient
+      .post('http://dev-lb-891765181.ap-southeast-1.elb.amazonaws.com:8080/api/admin/winners/select', campaignData)
+      .subscribe(
+        data => {
+          const res = JSON.parse(JSON.stringify(data));
+          console.log('POST Request is successful ', res);
+          if (res.status !== 'exception') {
+            // setTimeout('', 5000);
+            this.wait(5000);
+            this.bgBlur = false;
+            switch (positionVal) {
+              case 1:
+                this.result.position1 = res.data;
+                break;
+              case 2:
+                this.result.position2 = res.data;
+                break;
+              case 3:
+                this.result.position3 = res.data;
+                break;
+              case 4:
+                this.result.position4 = res.data;
+                break;
+              case 5:
+                this.result.position5 = res.data;
+                break;
+            }
+          } else {
+            this.bgBlur = false;
+            confirm('Please try again later');
+          }
+        },
+        error  => {
+        console.log('Error', error);
+        }
+      );
+  }
+  wait(ms){
+    const start = new Date().getTime();
+    let end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
 }
